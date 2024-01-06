1a8caa089b2699c151742fe2e411c2eeaf1bd6fe
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2024-01-06 11:08:31 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         long a=sc.nextInt();         long b=sc.nextInt();         double c=(a-b)/(a*1.0);         System.out.printf("%.2f",c*100);     } }

---
+++ Loss_Percentage.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextInt();
+        long b=sc.nextInt();
+        double c=(a-b)/(a*1.0);
+        System.out.printf("%.2f",c*100);
+    }
+}
