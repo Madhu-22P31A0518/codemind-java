e05688b834c3b2a7e5c1d5167580b97b6ea73066
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2024-01-06 11:43:55 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         double c=5*a/18.0;         System.out.printf("%.2f",c);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double c=5*a/18.0;
+        System.out.printf("%.2f",c);
+    }
+}
