2112e8b2336495b44305d226b6abd96eeb866e34
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2024-01-07 20:43:29 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int d=c-b,e=d/a;         System.out.print(e);     } }

---
+++ Can_Cross_the_Bridge.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=c-b,e=d/a;
+        System.out.print(e);
+    }
+}
