04251d10cf961ecfec54aeefa531a6ef3470a371
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2024-01-21 16:59:33 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a>40)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>40)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
