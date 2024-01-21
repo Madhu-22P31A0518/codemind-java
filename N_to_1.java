75c96710e783b1a1d4cc0e951366865d3ed8445b
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2024-01-21 22:43:56 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         while(a!=0)         {             System.out.printf("%d ",a);             a--;         }     } }

---
+++ N_to_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        while(a!=0)
+        {
+            System.out.printf("%d ",a);
+            a--;
+        }
+    }
+}
